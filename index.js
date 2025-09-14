6e97ade05ff6d4c9338b016ceb03eebecc5db9eb
pkredhat <120052595+pkredhat@users.noreply.github.com>
2025-09-13 23:26:00 -0400

    updated graph

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import pg from 'pg';
-import fs from 'fs';
+import fs from 'fs'; 
 import { PubSub } from 'graphql-subscriptions';  // to simulate real-time events
 
 
